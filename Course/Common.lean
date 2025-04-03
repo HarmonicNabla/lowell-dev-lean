8be601cda3743061d0e491e9a3aef3c655bcd827
Joris Roos <roos.jor@gmail.com>
2025-04-03 09:04:17 -0400

    week 8

--- Course/Common.lean
+++ Course/Common.lean
@@ -1,2 +1,11 @@
 import Mathlib.Tactic -- imports some portion of Mathlib
 import Mathlib.Util.Delaborators
+import Mathlib.Analysis.SpecialFunctions.Integrals
+
+set_option linter.unusedTactic false
+
+namespace Course
+
+noncomputable abbrev length := @MeasureTheory.volume ℝ inferInstance
+
+end Course
---
+++ Course/Homework/HW8.lean
@@ -0,0 +1,20 @@
+import Course.Common
+import Mathlib.Analysis.SpecialFunctions.Integrals
+
+set_option linter.unusedTactic false
+/-
+
+Homework sheet 8
+Due Apr 10 10am
+
+All problems are mandatory unless stated otherwise.
+
+Optional: Do all the problems in MIL Ch. 12.1
+
+-/
+
+namespace HW8
+
+
+
+end HW8
